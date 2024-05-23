e9bc193c13c16d11e74f1424b831fbc0c2b7d4d6
Michael Caballero <caballmi@oregonstate.edu>
2024-05-23 03:30:55 -0700

    Create JS file to connect MySQL DB to app

---
+++ database/db-connector.js
@@ -0,0 +1,16 @@
+/* ./database/db-connector.js */
+
+// get an instance of mysql we can use in the app
+const mysql = require('mysql');
+
+// create a 'connection pool' using the provided credentials
+const pool = mysql.createPool({
+	connectionLimit : 10,
+	host		: 'classmysql.engr.oregonstate.edu',
+	user		: 'cs340_caballmi',
+	password	: '2335',
+	database	: 'cs340_caballmi'
+});
+
+// export the db for use in the app
+module.xports.pool = pool;
